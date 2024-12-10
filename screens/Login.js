a9869e0ebe98aa97d781d6d18a3f2d41260b8828
Luis Renato <lrpfeliciano@gmail.com>
2024-12-10 15:35:40 -0300

    Finalzinho do login

--- screens/Login.js
+++ screens/Login.js
@@ -1,19 +1,23 @@
 import React, {useState} from 'react';
-import { View, Text, TextInput, Button} from 'react-native';
+import { View, Text, TextInput, Button, Alert} from 'react-native';
 
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
 
+    const executarLogin = () => {
+        Alert.alert("Olá", 'Usuário: '+ email );
+    };
+
     return (
         <View>
             <Text>E-mail</Text>
-            <TextInput value="{email}" />
+            <TextInput value={email} onChangeText={setEmail} />
             <Text>Senha</Text>
-            <TextInput value="{senha}" secureTextEntry />
+            <TextInput value={senha} onChangeText={setSenha} secureTextEntry />
 
-            <Button title="Entrar" />
+            <Button title="Entrar" onPress={executarLogin}/>
         </View>
 
     );
