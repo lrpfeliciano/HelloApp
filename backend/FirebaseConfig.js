33ecf4d26683271f845029f566b379e9453f4069
Luis Renato <lrpfeliciano@gmail.com>
2024-12-12 18:12:40 -0300

    Conexão firebase e login

---
+++ backend/FirebaseConfig.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+import { signInWithEmailAndPassword, initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAfIKjvwRS3R8ysp1TFoo1J0KvzXDRcfB8",
+  authDomain: "helloapp-102f7.firebaseapp.com",
+  projectId: "helloapp-102f7",
+  storageBucket: "helloapp-102f7.firebasestorage.app",
+  messagingSenderId: "572475404390",
+  appId: "1:572475404390:web:c23d9457aa5732f3d4bda7",
+  measurementId: "G-L5GNRP7W0H"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+//const auth = getAuth(app);
+const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
+
+export {app, auth, signInWithEmailAndPassword};
--- screens/Login.js
+++ screens/Login.js
@@ -1,13 +1,18 @@
 import React, {useState} from 'react';
 import { View, Text, TextInput, Button, Alert} from 'react-native';
-
+import { auth, signInWithEmailAndPassword } from '../backend/FirebaseConfig';
 
 const LoginScreen = () => {
     const [email, setEmail] = useState('');
     const [senha, setSenha] = useState('');
 
-    const executarLogin = () => {
-        Alert.alert("Olá", 'Usuário: '+ email );
+    const executarLogin = async () => {
+        try {
+            await signInWithEmailAndPassword(auth, email, senha);
+            Alert.alert("Sucesso", 'Logado com sucesso' );
+        } catch(error) {
+            Alert.alert("Erro", error.message);
+        }
     };
 
     return (
